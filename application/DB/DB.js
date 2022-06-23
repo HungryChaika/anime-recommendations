baa080ef6aa2c60eb5a16bdc97cdf06eab9f6cfe
NikBel3476 <nikita.belkin3476@gmail.com>
2022-06-23 13:59:43 +0400

    fix: delete console.log

--- application/DB/DB.js
+++ application/DB/DB.js
@@ -2,13 +2,6 @@ const { Client } = require("pg");
 
 class DB {
   constructor() {
-    console.log(
-      process.env.DB_USER,
-      process.env.DB_HOST,
-      process.env.DB_NAME,
-      process.env.DB_PASSWORD,
-      process.env.DB_PORT
-    );
     const client = new Client({
       user: process.env.DB_USER,
       host: process.env.DB_HOST,
